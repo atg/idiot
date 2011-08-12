45ac2bb7c4743a4e3497dbbe134541b4cd42da7a
Alex Gordon <alextgordon@gmail.com>
2011-08-12 23:51:57 +0100

    second comment (!)

---
+++ Testing/test.coffee
@@ -0,0 +1,19 @@
+#; foo: bar
+#; lorem ipsum dolor
+#;  sit amet
+#;  blah blah blah
+#;    blah blah blah
+#; sit amet
+#;   blah blah
+#;   blah
+foo()
+
+##; bar: baz
+##; lorem ipsum dolor
+##;  sit amet
+##;  blah blah blah
+##;    blah blah blah
+##; sit amet
+##;   blah blah
+##;   blah
+baz()
--- idiot.py
+++ idiot.py
@@ -1,8 +1,5 @@
 import sys
 
-f = open(sys.argv[1])
-print render(parse((for line in f), ['#*', '##*', '###*', '####*']))
-
 def parse(line_iterator, hcomments):
 	items = []
 	current_item = {}
@@ -13,7 +10,7 @@ def parse(line_iterator, hcomments):
 		# Find out the "level" of the line, if it has one. Is it h1, h2, h3, ...?
 		level = 0
 		for i, hcomment in enumerate(hcomments):
-			if tline.startsWith(hcomment):
+			if tline.startswith(hcomment):
 				level = i + 1
 				start_comment = hcomment
 				break
@@ -46,10 +43,13 @@ def parse(line_iterator, hcomments):
 			current_item['level'] = level
 		
 		# If >= 2, this is an "example"
-		if whitespace_amount > 2:
+		elif whitespace_amount >= 2:
 			if not last_was_example:
 				current_item['description'] += '\n'
-			current_item['description'] += '    ' + trest + '\n'
+				current_item['first_whitespace'] = whitespace_amount
+			# Remove current_item['first_whitespace'] of whitespace
+			mtrest = rest[current_item['first_whitespace']:]
+			current_item['description'] += '    ' + mtrest + '\n'
 			last_was_example = True
 		# Otherwise it's a normal description line
 		else:
@@ -66,6 +66,9 @@ def parse(line_iterator, hcomments):
 def render(items):
 	s = ''
 	for item in items:
-		s += str.format('<h%d class="docitem"><span class="docname">%s</span> <code>%s</code></h%d>\n\n', items['level'], items['name'], items['prototype'], items['level'])
-		s += items['description']
+		s += '\n<h%d class="docitem"><span class="docname">%s</span> <code>%s</code></h%d>\n\n' % (item['level'], item['name'], item['prototype'], item['level'])
+		s += item['description']
 	return s
+
+f = open(sys.argv[1])
+print render(parse((line for line in f), ['#;', '##;', '###;', '####;']))
